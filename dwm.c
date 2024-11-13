dd508178096d6c70e9df7e4889a0a3406e8fdea4
p4rfait <p4rfait@proton.me>
2024-11-13 05:48:00 -0600

    fix topbar & warp

--- dwm.c
+++ dwm.c
@@ -1735,7 +1735,7 @@ setup(void)
 	lrpad = drw->fonts->h;
 	bh = drw->fonts->h + 2;
 	sp = sidepad;
-	vp = vertpad;
+	vp = (topbar == 1)? vertpad : - vertpad;
 	updategeom();
 
 	/* init atoms */
@@ -2008,14 +2008,14 @@ updatebarpos(Monitor *m)
 	m->wh = m->mh;
 	if (m->showbar) {
 		m->wh = m->wh - vertpad - bh;
-		m->by = m->topbar ? m->wy : m->wy + m->wh - vp;
+		m->by = m->topbar ? m->wy : m->wy + m->wh + vertpad;
 		m->wy = m->topbar ? m->wy + bh + vp : m->wy;
 	} else
 		m->by = -bh - vp;
 	if (m->extrabar) {
 		m->wh = m->wh - vertpad - bh;
-		m->eby = !m->topbar ? m->wy : m->wy + m->wh - vp;
-		m->wy = !m->topbar ? m->wy + bh + vp : m->wy;
+		m->eby = !m->topbar ? m->wy + bh: m->wy + m->wh - vertpad;
+		m->wy = !m->topbar ? m->wy + bh - vp: m->wy;
 	} else
 		m->eby = -bh - vp;
 }
@@ -2265,7 +2265,7 @@ warp(const Client *c)
 		 y > c->y - c->bw &&
 		 x < c->x + c->w + c->bw*2 &&
 		 y < c->y + c->h + c->bw*2) ||
-		(y > c->mon->by && y < c->mon->by + bh) ||
+		(y > c->mon->by + vp && y < c->mon->by + bh + vp) ||
 		(c->mon->topbar && !y))
 		return;
 
