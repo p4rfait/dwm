1f79db679e155a80319d5bde71268b2d18f1fe2e
p4rfait <p4rfait@proton.me>
2024-11-21 14:12:14 -0600

    bar height

--- config.def.h
+++ config.def.h
@@ -11,6 +11,7 @@ static const unsigned int gappov    = 10;       /* vert outer gap between window
 static       int smartgaps          = 0;        /* 1 means no outer gap when there is only one window */
 static const int showbar            = 1;        /* 0 means no standard bar */
 static const int topbar             = 1;        /* 0 means standard bar at bottom */
+static const int user_bh            = 4;        /* 2 is the default spacing around the bar's font */
 static const int vertpad            = 10;       /* vertical padding of bar */
 static const int sidepad            = 10;       /* horizontal padding of bar */
 static const int extrabar           = 1;        /* 0 means no extra bar */
