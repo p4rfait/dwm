d31ef185d96a6f520eea2548541b93df5668e4c1
p4rfait <p4rfait@proton.me>
2024-12-09 20:05:53 -0600

    update run script

--- rundwm.sh
+++ rundwm.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-Xephyr -screen 1280x720 -reset -terminate -extension GLX 2>/dev/null :1 &
+Xephyr -screen 800x600+0+0 -screen 800x600+800+0 -reset -terminate -extension GLX +xinerama +extension RANDR 2>/dev/null :1 &
 export DISPLAY=:1
 sleep 0.1
 ./dwm
