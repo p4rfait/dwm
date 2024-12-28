a24cc966e42070b7967732e17465168757a495b8
p4rfait <p4rfait@proton.me>
2024-12-27 20:09:28 -0600

    actualfullscreen patch

--- dwm.c
+++ dwm.c
@@ -223,6 +223,7 @@ static void tag(const Arg *arg);
 static void tagmon(const Arg *arg);
 static void togglebar(const Arg *arg);
 static void toggleextrabar(const Arg *arg);
+static void togglefullscr(const Arg *arg);
 static void togglefloating(const Arg *arg);
 static void toggletag(const Arg *arg);
 static void toggleview(const Arg *arg);
@@ -1898,6 +1899,13 @@ tagmon(const Arg *arg)
 	if (!selmon->sel || !mons->next)
 		return;
 	sendmon(selmon->sel, dirtomon(arg->i));
+}
+
+void
+togglefullscr(const Arg *arg)
+{
+  if(selmon->sel)
+    setfullscreen(selmon->sel, !selmon->sel->isfullscreen);
 }
 
 void
