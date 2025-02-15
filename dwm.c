57892bad4e2693c5815a3dd9d55989170e8150e9
p4rfait <p4rfait@proton.me>
2025-02-15 13:05:38 -0600

    minnor changes

--- dwm.c
+++ dwm.c
@@ -837,7 +837,7 @@ drawstatusbar(Monitor *m, int bh, int left, char* stext) {
 					while (text[++i] != ',');
 					int rh = atoi(text + ++i);
 
-					drw_rect(drw, rx + x, ry + y, rw, rh, 1, 0);
+					drw_rect(drw, rx + x, ry, rw, rh, 1, 0);
 				} else if (text[i] == 'f') {
 					x += atoi(text + ++i);
 				}
