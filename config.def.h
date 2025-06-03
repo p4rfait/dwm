3147564488dd2cb4c5d069322f3f3b8e759965a3
p4rfait <p4rfait@proton.me>
2025-06-02 19:18:44 -0600

    patch: movestack

--- config.def.h
+++ config.def.h
@@ -106,6 +106,7 @@ static const char *audioprev[]         = { "/usr/bin/playerctl", "previous", NUL
 static const char *audionext[]         = { "/usr/bin/playerctl", "next", NULL };
 static const char *clipboard[]         = { "clipcat-menu", NULL };
 
+#include "movestack.c"
 static const Key keys[] = {
 	/* modifier                     key                       function        argument */
 	{ MODKEY,                       XK_p,                     spawn,          {.v = dmenucmd } },
@@ -117,6 +118,8 @@ static const Key keys[] = {
 	{ MODKEY,                       XK_d,                     incnmaster,     {.i = -1 } },
 	{ MODKEY,                       XK_h,                     setmfact,       {.f = -0.05} },
 	{ MODKEY,                       XK_l,                     setmfact,       {.f = +0.05} },
+	{ MODKEY|ShiftMask,             XK_j,                     movestack,      {.i = +1 } },
+	{ MODKEY|ShiftMask,             XK_k,                     movestack,      {.i = -1 } },
 	{ MODKEY|ShiftMask,             XK_h,                     setcfact,       {.f = +0.25} },
 	{ MODKEY|ShiftMask,             XK_l,                     setcfact,       {.f = -0.25} },
 	{ MODKEY|ShiftMask,             XK_o,                     setcfact,       {.f =  0.00} },
