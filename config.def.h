fe13748080cfaa8e54c92214913b46702da42ed3
p4rfait <p4rfait@proton.me>
2025-08-04 10:14:04 -0600

    Config: Change font to Iosevka

--- config.def.h
+++ config.def.h
@@ -11,8 +11,8 @@ static const unsigned int gappov    = 8;       /* vert outer gap between windows
 static       int smartgaps          = 0;        /* 1 means no outer gap when there is only one window */
 static const int showbar            = 1;        /* 0 means no bar */
 static const int topbar             = 1;        /* 0 means bottom bar */
-static const char *fonts[]          = { "JetBrainsMono Nerd Font:size=9:antialias=true:autohint=true" };
-static const char dmenufont[]       = "JetBrainsMono Nerd Font:size=9:antialias=true:autohint=true";
+static const char *fonts[]          = { "Iosevka Nerd Font:size=10:antialias=true:autohint=true" };
+static const char dmenufont[]       = "Iosevka Nerd Font:size=10:antialias=true:autohint=true";
 
 /* Rose Pine colorscheme */
 static const char col_base[]          = "#191724";
@@ -45,7 +45,7 @@ static const char etagf[] = "[%s]";	/* format of an empty tag */
 static const int lcaselbl = 0;		/* 1 means make tag label lowercase */	
 
 static const unsigned int ulinepad	= 5;	/* horizontal padding between the underline and tag */
-static const unsigned int ulinestroke	= 2;	/* thickness / height of the underline */
+static const unsigned int ulinestroke	= 1;	/* thickness / height of the underline */
 static const unsigned int ulinevoffset	= 0;	/* how far above the bottom of the bar the line should appear */
 static const int ulineall 		= 0;	/* 1 to show underline on all tags, 0 for just the active ones */
 
