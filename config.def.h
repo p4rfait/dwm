001a9c45b42afa01ffded0d08a9d95e8b2b3dc24
p4rfait <p4rfait@proton.me>
2025-08-18 21:39:45 -0600

    Config: Remove unused variables

--- config.def.h
+++ config.def.h
@@ -10,8 +10,6 @@ static const unsigned int gappov    = 8;       /* vert outer gap between windows
 static       int smartgaps          = 0;        /* 1 means no outer gap when there is only one window */
 static const int showbar            = 1;        /* 0 means no bar */
 static const int topbar             = 1;        /* 0 means bottom bar */
-static const int horizpadbar        = 0;        /* horizontal padding for statusbar */
-static const int vertpadbar         = 10;
 static const int vertpad            = 8;       /* vertical padding of bar */
 static const int sidepad            = 8;       /* horizontal padding of bar */
 static const char *fonts[]          = { "IosevkaTermSlab Nerd Font:size=12" };
